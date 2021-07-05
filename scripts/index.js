bf86171c0b1c968c92803fb5ccb52fd9da8de099
JacobHocker <jacobdouglas06@gmail.com>
2021-07-05 14:15:12 -0500

    prepped for pitch

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 console.log('Welcome to Advice For You!', 'color: red');
 
 const adviceTag = document.querySelector('#advice');
+const refreshButton = document.querySelector('#reload');
+
+refreshButton.addEventListener('click', () => {
+     getAdvice()
+});
 
 getAdvice()
 .then(data => {
